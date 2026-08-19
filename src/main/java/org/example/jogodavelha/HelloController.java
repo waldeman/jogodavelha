c09e6926aec8ad0ec4bd52263d666b195b5a65ba
Waldeman Alves Leite da Silva <waldemanleites@gmail.com>
2026-08-19 10:38:32 -0300

    JogoFuncionando
    A estrutura básica nesse momento está funcionado por completo

--- src/main/java/org/example/jogodavelha/HelloController.java
+++ src/main/java/org/example/jogodavelha/HelloController.java
@@ -2,13 +2,33 @@ package org.example.jogodavelha;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
     @FXML
-    private Button[][] botaos = new Button[3][3];
+    private GridPane tabuleiro;
 
+    private Button[][] botaos = new Button[3][3];
+    @FXML
+    public void initialize() {
+        for (Node node : tabuleiro.getChildren()){
+            if (node instanceof Button){
+                Button b = (Button) node;
+                Integer linha = GridPane.getRowIndex(b);
+                Integer coluna = GridPane.getColumnIndex(b);
+                if (linha == null){
+                    linha = 0;
+                }
+                if (coluna == null){
+                    coluna = 0;
+                }
+                botaos[linha][coluna] = b;
+            }
+        }
+    }
     @FXML
     public void jogar(ActionEvent event) {
         int[] pos = qualOBotao(botaos, event);
