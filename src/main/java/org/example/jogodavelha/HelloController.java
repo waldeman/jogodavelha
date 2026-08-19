c81ee49da30b7862eb89b534d3dc05b3c2721032
Waldeman Alves Leite da Silva <waldemanleites@gmail.com>
2026-08-19 12:10:23 -0300

    ParteLógica
    Verificador de vitórias

--- src/main/java/org/example/jogodavelha/HelloController.java
+++ src/main/java/org/example/jogodavelha/HelloController.java
@@ -10,10 +10,13 @@ import javafx.scene.layout.GridPane;
 public class HelloController {
     @FXML
     private GridPane tabuleiro;
-
+    @FXML
+    private Label labelResultado;
     private Button[][] botaos = new Button[3][3];
+    private String jogada;
     @FXML
     public void initialize() {
+        jogada = "X";
         for (Node node : tabuleiro.getChildren()){
             if (node instanceof Button){
                 Button b = (Button) node;
@@ -32,10 +35,45 @@ public class HelloController {
     @FXML
     public void jogar(ActionEvent event) {
         int[] pos = qualOBotao(botaos, event);
-        botaos[pos[0]][pos[1]].setText("A");
+        if (!ganhou()) {
+            if (isVazia(botaos, pos)) {
+                botaos[pos[0]][pos[1]].setText(jogada);
+                alternarJogada();
+            } else {
+                labelResultado.setText("Posição já preenchida!");
+            }
+        }
+
     }
+    public boolean ganhou(){
 
-
+        for (int i = 0; i < botaos.length; i++){
+            if (this.botaos[i][0].getText().equals(this.jogada) && this.botaos[i][1].getText().equals(this.jogada) && this.botaos[i][2].getText().equals(this.jogada)){
+                return true;
+            } else if (this.botaos[0][i].getText().equals(this.jogada) && this.botaos[1][0].getText().equals(this.jogada) && this.botaos[2][i].getText().equals(this.jogada)) {
+                return true;
+            }
+        }
+        if (this.botaos[0][0].getText().equals(this.jogada) && this.botaos[1][1].getText().equals(this.jogada) && this.botaos[2][2].getText().equals(this.jogada)){
+            return true;
+        }
+        if (this.botaos[0][2].getText().equals(this.jogada) && this.botaos[1][1].getText().equals(this.jogada) && this.botaos[2][0].getText().equals(this.jogada)){
+            return true;
+        }
+        return false;
+    }
+    public static boolean isVazia(Button[][] botoes,int[] pos){
+        if (botoes[pos[0]][pos[1]].getText().equals("")){
+            return true;
+        }return false;
+    }
+    public void alternarJogada(){
+        if (this.jogada.equals("X")){
+            this.jogada = "O";
+        }else if (this.jogada.equals("O")){
+            this.jogada = "X";
+        }
+    }
     public int[] qualOBotao(Button[][] botaos, ActionEvent evento) {
         Button certo = (Button) evento.getSource();
         int[] posicao = new int[2];
