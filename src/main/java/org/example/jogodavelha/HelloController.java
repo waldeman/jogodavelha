af7969bb3d5c7513a8f86e6c3de764b3dd83f3b5
Waldeman Alves Leite da Silva <waldemanleites@gmail.com>
2026-08-19 15:05:20 -0300

    Parte-lógica
    Terminei a parte lógica do programa

--- src/main/java/org/example/jogodavelha/HelloController.java
+++ src/main/java/org/example/jogodavelha/HelloController.java
@@ -14,72 +14,104 @@ public class HelloController {
     private Label labelResultado;
     private Button[][] botaos = new Button[3][3];
     private String jogada;
+
     @FXML
     public void initialize() {
         jogada = "X";
-        for (Node node : tabuleiro.getChildren()){
-            if (node instanceof Button){
+        for (Node node : tabuleiro.getChildren()) {
+            if (node instanceof Button) {
                 Button b = (Button) node;
                 Integer linha = GridPane.getRowIndex(b);
                 Integer coluna = GridPane.getColumnIndex(b);
-                if (linha == null){
+                if (linha == null) {
                     linha = 0;
                 }
-                if (coluna == null){
+                if (coluna == null) {
                     coluna = 0;
                 }
                 botaos[linha][coluna] = b;
             }
         }
     }
+
     @FXML
     public void jogar(ActionEvent event) {
         int[] pos = qualOBotao(botaos, event);
-        if (!ganhou()) {
-            if (isVazia(botaos, pos)) {
-                botaos[pos[0]][pos[1]].setText(jogada);
-                alternarJogada();
-            } else {
-                labelResultado.setText("Posição já preenchida!");
+
+
+        if (isVazia(botaos, pos)) {
+            botaos[pos[0]][pos[1]].setText(jogada);
+            if (ganhou()) {
+                labelResultado.setText(jogador() + " ganhou!");
             }
+            if (isVelha()) {
+                labelResultado.setText("Deu velha");
+            }
+            alternarJogada();
+        } else {
+            labelResultado.setText("Posição já preenchida!");
         }
+
 
     }
-    public boolean ganhou(){
 
-        for (int i = 0; i < botaos.length; i++){
-            if (this.botaos[i][0].getText().equals(this.jogada) && this.botaos[i][1].getText().equals(this.jogada) && this.botaos[i][2].getText().equals(this.jogada)){
+    public boolean isVelha() {
+        for (Button[] i : this.botaos) {
+            for (Button j : i) {
+                if (j.getText().isEmpty()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public String jogador() {
+        if (this.jogada.equals("X")) {
+            return "Jogador 1";
+        }
+        return "Jogador 2";
+    }
+
+    public boolean ganhou() {
+
+        for (int i = 0; i < botaos.length; i++) {
+            if (this.botaos[i][0].getText().equals(this.jogada) && this.botaos[i][1].getText().equals(this.jogada) && this.botaos[i][2].getText().equals(this.jogada)) {
                 return true;
-            } else if (this.botaos[0][i].getText().equals(this.jogada) && this.botaos[1][0].getText().equals(this.jogada) && this.botaos[2][i].getText().equals(this.jogada)) {
+            } else if (this.botaos[0][i].getText().equals(this.jogada) && this.botaos[1][i].getText().equals(this.jogada) && this.botaos[2][i].getText().equals(this.jogada)) {
                 return true;
             }
         }
-        if (this.botaos[0][0].getText().equals(this.jogada) && this.botaos[1][1].getText().equals(this.jogada) && this.botaos[2][2].getText().equals(this.jogada)){
+        if (this.botaos[0][0].getText().equals(this.jogada) && this.botaos[1][1].getText().equals(this.jogada) && this.botaos[2][2].getText().equals(this.jogada)) {
             return true;
         }
-        if (this.botaos[0][2].getText().equals(this.jogada) && this.botaos[1][1].getText().equals(this.jogada) && this.botaos[2][0].getText().equals(this.jogada)){
+        if (this.botaos[0][2].getText().equals(this.jogada) && this.botaos[1][1].getText().equals(this.jogada) && this.botaos[2][0].getText().equals(this.jogada)) {
             return true;
         }
         return false;
     }
-    public static boolean isVazia(Button[][] botoes,int[] pos){
-        if (botoes[pos[0]][pos[1]].getText().equals("")){
+
+    public static boolean isVazia(Button[][] botoes, int[] pos) {
+        if (botoes[pos[0]][pos[1]].getText().equals("")) {
             return true;
-        }return false;
+        }
+        return false;
     }
-    public void alternarJogada(){
-        if (this.jogada.equals("X")){
+
+    public void alternarJogada() {
+        if (this.jogada.equals("X")) {
             this.jogada = "O";
-        }else if (this.jogada.equals("O")){
+        } else if (this.jogada.equals("O")) {
             this.jogada = "X";
         }
     }
+
     public int[] qualOBotao(Button[][] botaos, ActionEvent evento) {
         Button certo = (Button) evento.getSource();
         int[] posicao = new int[2];
         for (int i = 0; i < botaos.length; i++) {
-            for (int j = 0;j < botaos.length; j++){
-                if (botaos[i][j] == certo){
+            for (int j = 0; j < botaos.length; j++) {
+                if (botaos[i][j] == certo) {
                     posicao[0] = i;
                     posicao[1] = j;
                 }
@@ -87,8 +119,4 @@ public class HelloController {
         }
         return posicao;
     }
-
-
-
-
 }
